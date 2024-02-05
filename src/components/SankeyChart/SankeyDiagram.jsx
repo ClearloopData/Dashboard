a29525abad6802b07b08c34a14f03d96ecf73935
Daniel <daniel.p.henricks@vanderbilt.edu>
2024-02-05 16:41:36 -0600

    Working on making the sankey diagram clickable

--- src/components/SankeyChart/SankeyDiagram.jsx
+++ src/components/SankeyChart/SankeyDiagram.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { Chart } from "react-google-charts";
 
 export const data = [
@@ -14,16 +17,64 @@ export const data = [
   ["Scope 3 Emissions", "Commuting", 5453],
 ];
 
-export const options = {};
+export const options = {
+  sankey: {
+    node: {
+      interactivity: true,
+    },
+  },
+};
 
 export function SankeyDiagram() {
+  const [selectedNode, setSelectedNode] = useState(null);
+
+  const handleNodeClick = (node) => {
+    console.log(node);
+    if (node[0] == "From" || node[0] == "To") {
+      return;
+    }
+    setSelectedNode(node);
+  };
+
   return (
-    <Chart
-      chartType="Sankey"
-      width="75%"
-      height="500px"
-      data={data}
-      options={options}
-    />
+    <Container>
+      <Chart
+        chartType="Sankey"
+        width="100%"
+        height="500px"
+        data={data}
+        options={options}
+        chartEvents={[
+          {
+            eventName: "ready",
+            callback: ({ chartWrapper, google }) => {
+              const chart = chartWrapper.getChart();
+              chart.container.addEventListener("click", (ev) =>
+                console.log(ev)
+              );
+            },
+          },
+        ]}
+      />
+      <Modal show={selectedNode !== null} onHide={() => setSelectedNode(null)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Node Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedNode && (
+            <div>
+              <p>From: {selectedNode[0]}</p>
+              <p>To: {selectedNode[1]}</p>
+              <p>Weight: {selectedNode[2]}</p>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setSelectedNode(null)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
   );
 }
