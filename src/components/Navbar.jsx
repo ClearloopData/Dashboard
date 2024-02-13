e34fcb0168ec405c97d304eeb3a413cab7912638
Daniel <daniel.p.henricks@vanderbilt.edu>
2024-02-13 15:20:38 -0600

    Finished routing +  navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,11 @@
+/**
+ * Navbar.jsx
+ * Implements a Navbar with the following structure:
+ * 1) Home
+ * 2) /data (Data)
+ * 3) A dropdown with 3 options to learn more.
+ */
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -11,29 +19,27 @@ function NavComponent() {
       style={{ backgroundColor: "#AEB3C4", fontFamily: "FiraCode" }}
     >
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="./">
           <img
             src={logo}
             height="30"
             className="d-inline-block align-top"
-            alt="React Bootstrap logo"
+            alt="Clearloop + Vandy Logo"
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Data">Data</Nav.Link>
-            <NavDropdown title="More Resources" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Start here!
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">FAQs</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
+            <Nav.Link href="./">Home</Nav.Link>
+            <Nav.Link href="/data">Data</Nav.Link>
+            <NavDropdown title="Resources" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/resources">Start here!</NavDropdown.Item>
+              <NavDropdown.Item href="#dropdown/3.2">FAQs</NavDropdown.Item>
+              <NavDropdown.Item href="https://clearloop.us">
                 Clearloop website
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="https://vanderbilt.edu/sustainability">
                 Vanderbilt sustainability website
               </NavDropdown.Item>
             </NavDropdown>
