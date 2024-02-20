274cceed0637614a3cd6ebe1ad7cfb64f8e3e7e1
Daniel <daniel.p.henricks@vanderbilt.edu>
2024-02-20 15:47:00 -0600

    Quick changes before deployment

--- apis/watttime.py
+++ apis/watttime.py
@@ -1,23 +1,42 @@
 # Code by Daniel Henricks
+# Todo: convert this to AWS lambda_function.
 
 import os
 from dotenv import load_dotenv, find_dotenv
-
+import firebase_admin
+from firebase_admin import db, credentials
 import requests
 from requests.auth import HTTPBasicAuth
+
 login_url = 'https://api.watttime.org/login'
 load_dotenv(find_dotenv())
 PASSWORD = os.environ.get("WATTTIME_PASSWORD")
+final_res = {}
 rsp = requests.get(login_url, auth=HTTPBasicAuth('clearloop', PASSWORD))
 TOKEN = rsp.json()['token']
-url = "https://api.watttime.org/v3/historical"
+url = "https://api.watttime.org/v3/forecast"
 headers = {"Authorization": f"Bearer {TOKEN}"}
-params = {
-    "region": "CAISO_NORTH",
-    "start": "2024-02-15T00:00+00:00",
-    "end": "2024-02-15T00:05+00:00",
-    "signal_type": "health_damage",
-}
-response = requests.get(url, headers=headers, params=params)
-response.raise_for_status()
-print(response.json())
+
+for i in ("TVA", "ERCOT_NORTHCENTRAL", "CAISO_NORTH"):
+    params = {
+        "region": i,
+        "signal_type": "co2_moer",
+        "horizon_hours": 0
+    }
+    response = requests.get(url, headers=headers, params=params)
+    final_res[i] = response.json()["data"][0]["value"]
+
+
+cred = credentials.Certificate("apis/watttime_database_config.json")
+firebase_admin.initialize_app(cred, 
+    {
+    'databaseURL': "https://clearloop-watttime-default-rtdb.firebaseio.com/"
+    }
+                              )
+   
+ref = db.reference("/moer")
+ref.push(final_res)
+
+# The final_res array will take the form of (example): 
+# {'TVA': 1177.6, 'ERCOT_NORTHCENTRAL': 653.2, 'CAISO_NORTH': 1012.9}
+
